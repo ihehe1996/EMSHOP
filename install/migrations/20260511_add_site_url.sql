7f8bc817332517ea497f05ef649e114dbbbf93c2
驳手 <ihehe1996@gmail.com>
2026-07-12 20:29:46 +0800

    删除无用文件

--- install/migrations/20260511_add_site_url.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `__PREFIX__config` (`config_name`, `config_value`, `description`)
-SELECT 'site_url', '', '站点地址（含 http(s)）'
-WHERE NOT EXISTS (
-    SELECT 1 FROM `__PREFIX__config` WHERE `config_name` = 'site_url'
-);
--- install/migrations/20260519120000_order_guest_contact.sql
+++
@@ -1,2 +0,0 @@
--- v1.3.01版本改动
-ALTER TABLE `__PREFIX__order` ADD COLUMN `guest_contact` VARCHAR(255) DEFAULT NULL COMMENT '游客联系方式（查单凭据）' AFTER `contact_info`;
