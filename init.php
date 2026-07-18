8902285473020886b7c4c4366ffd8c7739ca10cc
驳手 <ihehe1996@gmail.com>
2026-07-19 00:49:30 +0800

    发布版本 v1.3.10

--- init.php
+++ init.php
@@ -30,8 +30,8 @@ if (!defined('EM_ROOT')) {
     define('EM_ROOT', __DIR__);
 }
 define('EM_INITIALIZED', true);
-define('EM_VERSION', '1.3.09');
-define('EM_VERSION_TIMESTAMP', '1309');
+define('EM_VERSION', '1.3.10');
+define('EM_VERSION_TIMESTAMP', '1310');
 
 require EM_ROOT . '/base.php';
 
