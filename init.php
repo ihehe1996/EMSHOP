0156ca7425a9e44078076f73eb3f5f19cd692045
驳手 <ihehe1996@gmail.com>
2026-07-26 15:22:08 +0800

    更新版本 v1.3.12

--- init.php
+++ init.php
@@ -30,8 +30,8 @@ if (!defined('EM_ROOT')) {
     define('EM_ROOT', __DIR__);
 }
 define('EM_INITIALIZED', true);
-define('EM_VERSION', '1.3.11');
-define('EM_VERSION_TIMESTAMP', '1311');
+define('EM_VERSION', '1.3.12');
+define('EM_VERSION_TIMESTAMP', '1312');
 
 require EM_ROOT . '/base.php';
 
